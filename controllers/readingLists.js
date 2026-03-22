8ac78ba246901bc80a87ac3f289a27bd66600dea
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-03-22 10:54:40 +0200

    13.23 user can update reading list entries on whether they've read the blog or not

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -4,6 +4,20 @@ const { SECRET } = require('../util/config')
 const { Blog, User } = require('../models')
 const { Op } = require('sequelize')
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
 /*
 Post to the reading list table with the following format:
 {
@@ -47,6 +61,44 @@ router.post('/', async (req, res, next) => {
     })
 
     res.status(201).json({ message: 'Blog added to reading list' })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  const { id } = req.params
+  const { blog_read } = req.body
+
+  if (blog_read === undefined) {
+    const error = new Error('blog_read field required')
+    error.status = 400
+    return next(error)
+  }
+
+  try {
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.findByPk(id)
+
+    if (!user || !blog) {
+      const error = new Error('User or Blog not found')
+      error.status = 404
+      return next(error)
+    }
+
+    // Check if blog exists in user's reading list
+    const exists = await user.hasReadingList(blog)
+    if (!exists) {
+      const error = new Error('Blog not in reading list')
+      error.status = 400
+      return next(error)
+    }
+
+    await user.addReadingList(blog, {
+      through: { blog_read }
+    })
+
+    res.json({ message: 'Reading status updated' })
   } catch (error) {
     next(error)
   }
